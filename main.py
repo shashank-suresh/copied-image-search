df6d289d70ff646d5f246a6e1bbea65caac40624
shashank-suresh <shashanksuresh3@gmail.com>
2023-01-01 22:40:17 +0530

    Added entry function upon hotkey press

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import keyboard
 
+def main():
+    """Entry point upon hotkey press"""
+    print("Hotkey pressed")
+
+
 # Basic functionality for copy hotkey
-keyboard.add_hotkey("Alt+C", lambda: print("Alt+C pressed"))
+keyboard.add_hotkey("Alt+C", main)
 keyboard.wait()
