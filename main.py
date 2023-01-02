c193bd90726beeede0457100e3fa8b5998985d36
shashank-suresh <shashanksuresh3@gmail.com>
2023-01-02 22:43:07 +0530

    Get current clipboard image (working fix)

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import keyboard
+from PIL import ImageGrab, Image
 
 def main():
     """Entry point upon hotkey press"""
     print("Hotkey pressed")
+
+    # Get the current clipboard image
+    img = ImageGrab.grabclipboard()
 
 
 # Basic functionality for copy hotkey
